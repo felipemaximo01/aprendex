f0a24f361f50a8dc6f42866ccad1a70e5ca91050
Felipe Maximo <felipecontato2001@hotmail.com>
2023-12-08 17:16:10 -0300

    projeto criado

---
+++ src/main/java/com/devmaximo/aprendex/AprendexApplication.java
@@ -0,0 +1,13 @@
+package com.devmaximo.aprendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendexApplication.class, args);
+	}
+
+}
